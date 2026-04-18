1447a9a8c907d61005535734c568350a4ceba038
Mario <Mario>
2026-04-18 23:45:33 +0200

    Canvi a la pagina dinici

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: 'https://la-teva-app.vercel.app/auth/callback',
+        emailRedirectTo: 'https://biblioteca-ccc.vercel.app/auth/callback',
       },
     })
 
